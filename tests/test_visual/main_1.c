cc330b34d2dfe19474873da398ad4b86236f42dc
Yudha Styawan <yudhastyawanjilid2@gmail.com>
2021-07-17 23:38:17 +0800

    updating visual #10

--- tests/test_visual/main_1.c
+++ tests/test_visual/main_1.c
@@ -3,7 +3,7 @@
 
 int main() {
     int size = 100;
-    AgxVector *vecx = agx_vector_new_linspace(1., 10., size);
+    AgxVector *vecx = agx_vector_new_linspace(0., 10., size);
     AgxVector *vecy = agx_vector_new_random(size, -10., 10.);
     
     agx_vector_print_partial(vecx);

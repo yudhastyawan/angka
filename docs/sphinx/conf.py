e199e1dccd62f87642d64cf4de2e7bd44bbe889c
Yudha Styawan <yudhastyawanjilid2@gmail.com>
2021-07-22 01:31:20 +0800

    testing without gitlog #17

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import subprocess
-subprocess.call('bash ./git-log.sh', shell=True)
+# subprocess.call('sh ./git-log.sh', shell=True)
 subprocess.call('cd .. ; doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------

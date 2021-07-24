75aef95b925c02c121b191913e84e97c247410c0
Yudha Styawan <yudhastyawanjilid2@gmail.com>
2021-07-24 15:03:20 +0800

    finishing vector.h documentation #17

--- include/vector.h
+++ include/vector.h
@@ -98,26 +98,118 @@ void *agx_vector_print_partial(AgxVector *vec);
  * @details it is based on @ref agx_vector_print with islong = 1
  * */
 void *agx_vector_print_full(AgxVector *vec);
+
+/**
+ * @brief adding all elements of vector by a value
+ * */
 void *agx_vector_add_by_value(AgxVector *vec, double val);
+
+/**
+ * @brief substracting all elements of vector by a value
+ * */
 void *agx_vector_substract_by_value(AgxVector *vec, double val);
+
+/**
+ * @brief multiplying all elements of vector by a value
+ * */
 void *agx_vector_multiply_by_value(AgxVector *vec, double val);
+
+/**
+ * @brief deviding all elements of vector by a value
+ * */
 void *agx_vector_devide_by_value(AgxVector *vec, double val);
+
+/**
+ * @brief changing all elements of vector by a value
+ * */
 void *agx_vector_change_elements_by_value(AgxVector *vec, double val);
+
+/**
+ * @brief create @ref agx_vector_new with a constant value
+ * */
 AgxVector *agx_vector_new_constant(int size, double val);
+
+/**
+ * @brief create @ref agx_vector_new by using the size of another vector
+ * */
 AgxVector *agx_vector_new_duplicate_size(AgxVector *vec);
+
+/**
+ * @brief copy all elements of a vector to a target vector
+ * @note both vectors must be the same size
+ * */
 void *agx_vector_copy_elements(AgxVector *src, AgxVector *target);
+
+/**
+ * @brief create @ref agx_vector_new by copying another vector
+ * */
 AgxVector *agx_vector_new_copy(AgxVector *vec);
+
+/**
+ * @brief create @ref agx_vector_new_constant with zero value
+ * */
 AgxVector *agx_vector_new_zero(int size);
+
+/**
+ * @brief create a new range vector by using the size value
+ * */
 AgxVector *agx_vector_new_linspace(double min, double max, int size);
+
+/**
+ * @brief create a new range vector using the interval value
+ * @param step the interval value
+ * @note the input is integer and the output is a double array
+ * */
 AgxVector *agx_vector_new_arange(int min, int step, int max);
+
+/**
+ * @brief create a new range vector using the interval value
+ * @param step the interval value
+ * @note the input is double and the output is a double array
+ * */
 AgxVector *agx_vector_new_arange_double(double min, double step, double max);
+
+/**
+ * @brief looking for the minimum value of vector
+ * */
 double agx_vector_min(AgxVector *vec);
+
+/**
+ * @brief looking for the maximum value of vector
+ * */
 double agx_vector_max(AgxVector *vec);
+
+/**
+ * @brief create a vector using the varadic values
+ * @note the size and number of values must be the same
+ * */
 AgxVector *agx_vector_new_values(int size, ... );
+
+/**
+ * @brief change each element of vector based on a certain function
+ * @note the input of function should be only one and double pointer
+ * */
 void *agx_vector_input_function(AgxVector *vec, void (*function)(double *));
+
+/**
+ * @brief the sinus of each element of the vector
+ * */
 void *agx_vector_sin(AgxVector *vec);
+
+/**
+ * @brief the double sinus
+ * */
 void agx_sin(double *val);
+
+/**
+ * @brief checking a value is exist on the vector in a certain indexes range
+ * @note index i where idx1 <= i <= idx2
+ * */
 int agx_vector_isexist(AgxVector *vec, int idx1, int idx2, double val);
+
+/**
+ * @brief sorting the elements of vector from minimum to maximum
+ * */
 void *agx_vector_sort(AgxVector *vec);
 
 #ifdef __cplusplus
